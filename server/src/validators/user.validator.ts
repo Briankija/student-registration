e81a294f2c0b3dc932098e564e16581da2a1e235
davisrya <ryandavis1722@gmail.com>
2025-06-25 14:59:35 +0300

    add validator

---
+++ server/src/validators/user.validator.ts
@@ -0,0 +1,12 @@
+import { body } from "express-validator";
+
+export const registerValidator = [
+    body('username').notEmpty().withMessage('Username is requires'),
+    body('registrationNo').notEmpty().withMessage('Registration Number is required'),
+    body('email').isEmail().withMessage('Valid email is required'),
+    body('password').isLength({ min: 6}).withMessage('Password must be at least 6 characters'),
+    body('confirmPassword').custom((value, { req }) => {
+        if(value !== req.body.password) throw new Error('Passwords do not match!');
+        return true;
+    })
+]
