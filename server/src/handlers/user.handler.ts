105810ba85440a458154c8d294bc7a4b6c144d5d
davisrya <ryandavis1722@gmail.com>
2025-06-26 18:20:09 +0300

    add sigin function

--- server/src/handlers/user.handler.ts
+++ server/src/handlers/user.handler.ts
@@ -1,7 +1,9 @@
 import { Request, Response, NextFunction } from "express";
 import prisma from "../db";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
+//registration
 export const registerUser = async (req: Request, res: Response, next: NextFunction) => {
 	const { username, registrationNo, email, password } = req.body;
 	const hashedPassword = await bcrypt.hash(password, 10);
@@ -16,8 +18,48 @@ export const registerUser = async (req: Request, res: Response, next: NextFuncti
 			},
 		});
 
+		const token = jwt.sign(
+			{ userId: user.id},
+			process.env.JWT_SECRET as string,
+			{ expiresIn: "1h"}
+		)
+
 		res.status(201).json({ message: "User registered successfully", user });
 	} catch (error) {
 		res.status(500).json({ error: "User registration failed", details: error });
 	}
 };
+
+//signin
+export const signInUser = async (req: Request, res: Response) => {
+	const {usernameOrEmail, password} = req.body;
+
+	try {
+	const user = await prisma.register.findFirst({
+		where: {
+			OR: [ 
+				{ username: usernameOrEmail },
+				{ email: usernameOrEmail } 
+			],
+		},		
+	})
+
+	if(!user) {
+		return res.status(400).json({ error: "Invalid credentials" });
+	}
+
+	const isPasswordValid = await bcrypt.compare(password, user.password);
+	if(isPasswordValid) {
+		return res.status(401).json({ error: "Incorrect password"});
+	}
+
+	const token = jwt.sign(
+		{ userId: user.id },
+		process.env.JWT_SECRET as string,
+	);
+
+	res.status(200).json({ message: "Sign in successful", token });
+	} catch (error) {
+		res.status(500).json({ error: "Sign in failed", details: error });
+	}
+}
